60bf47b66404d9504e80a91bf3bed534618e5a4e
unknown <adilamarcele@hotmail.com>
2024-04-09 10:20:51 -0300

    Refatoraçao função checkGuess e main

--- src/main.py
+++ src/main.py
@@ -1,36 +1,24 @@
 import random
 import time
 
-
-# Classe pai para os jogadores
 class Player:
     def __init__(self, name):
         self.name = name
-        self.guesses = []  #Lista para armazenar os palpites dos jogadores
+        self.guesses = []
 
     def make_guess(self):
-        pass  #Método a ser implementado pelas classes filhas
+        pass
 
-#Classe para o jogador computador
 class ComputerPlayer(Player):
     def make_guess(self):
-        # O computador faz um palpite aleatório entre 1 e 100
-        bet = random.randint(1, 100)
-        self.guesses.append(bet)  # Armazena o palpite na lista de palpites
-        return bet
-
-# Classe para o jogador humano
-
+        return random.randint(1, 100)
 
 class HumanPlayer(Player):
     def make_guess(self):
         while True:
             try:
-                # O jogador humano digita seu 
-                #input('Você começa!')
-                bet = int(input('Player, digite seu palpite: '))
+                bet = int(input('{}, digite seu palpite: '.format(self.name)))
                 if 1 <= bet <= 100:
-                    # Armazena o palpite válido na lista de palpites
                     self.guesses.append(bet)
                     return bet
                 else:
@@ -38,84 +26,60 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Por favor, digite um número válido.')
 
-# Função para iniciar o jogo com um número aleatório entre 1 e 100
+def check_guess(player, target_number):
+    bet = player.make_guess()
+    print(player.name + ', seu palpite foi:', bet)
 
+    if bet == target_number:
+        print('Parabéns {}, você acertou! O número correto é: {}'.format(player.name, bet))
+        display_game_result(player)
+        return True
+    elif bet < target_number:
+        print('Muito baixo!\n')
+    else:
+        print('Muito alto!\n')
+
+    return False
+
+def switch_player(current_player):
+    if isinstance(current_player, HumanPlayer):
+        return ComputerPlayer('Computador The Boss')
+    else:
+        return HumanPlayer('Jogador')
+
+def display_game_result(winner):
+    print('RESUMO DA PARTIDA!')
+    print('{} Aqui vai o resumo da partida.'.format(winner.name))
+    print('Tentativas de {}: {}'.format(winner.name, len(winner.guesses)))
+    print('{} seus palpites foram: {}'.format(winner.name, ' '.join(map(str, winner.guesses))))
 
 def start_game():
     return random.randint(1, 100)
 
-# Função para verificar o palpite do jogador e determinar se ele acertou ou não
-
-
-def check_guess(player, target_number, max_attempts):
-    computer_player = ComputerPlayer('Computador The Boss')
-    attempts = 0
-
-    while attempts < max_attempts:
-        bet = player.make_guess()  # O jogador faz um palpite
-        print(player.name + ' seu palpite foi:', bet)
-        attempts += 1
-
-        if bet == target_number:
-            print('UHUU VOCÊ CONSEGUIU ' + player.name +
-                  '! ' + 'O número correto é:', bet)
-            display_game_result(player)
-            return
-        elif bet < target_number:
-            print('Muito baixo!')
-        else:
-
-            print('Muito alto!')\
-
-       # Introduzindo uma pausa de 1 segundo antes de alternar para o próximo jogador
-        time.sleep(1)
-
-        print()
-        # Alternância entre o jogador e o computador
-        player, computer_player = computer_player, player
-
-    if attempts >= max_attempts:
-        print('Fim de jogo! O número de tentativas máxima foi alcançada.')
-        display_game_result(computer_player)
-        
-
-# Função para exibir o resumo da partida
-
-
-def display_game_result(winner):
-    print('RESUMO DA PARTIDA!')
-    print(winner.name + ' Aqui vai o resumo da partida.')
-    print('Tentativas de ' + winner.name + ': ' + str(len(winner.guesses)))
-    bet_resume(winner)
-
-# Função para exibir os palpites feitos pelo jogador
-
-
-def bet_resume(player):
-    print(player.name + ' seus palpites foram:', end=' ')
-    print('!'.join(map(str, player.guesses)) + '!')
-
-# Função principal do jogo
-
-
 def main():
     target_number = start_game()
-    print()
-    print('BEM VINDO AO GUESS THE NUMBER!')
-    print()
-    print('O número foi sorteado. São dez rodadas. Tente acertá-lo.')
+    
+    print('BEM VINDO AO GUESS THE NUMBER!\n')
+    print('O número foi sorteado. São dez rodadas. Tente acertá-lo.\n')
     print('O Computador é o seu adversário.')
     print('Pressione Enter para começar ou digite qualquer outra coisa para sair.')
 
     input_text = input()
     if input_text == '':
         player = HumanPlayer('Jogador')
-        max_guesses = 10  # Número máximo de tentativas permitidas
-        check_guess(player, target_number, max_guesses)
+        attempts = 0
+        max_attempts = 10
+        while attempts < max_attempts:
+            if check_guess(player, target_number):
+                return
+            player = switch_player(player)
+            attempts += 1
+
+        print('Fim de jogo! O número máximo de tentativas foi alcançado.')
+        display_game_result(switch_player(player))
     else:
         print('Reiniciando o jogo...')
         main()
-
 
 if __name__ == '__main__':
     main()
